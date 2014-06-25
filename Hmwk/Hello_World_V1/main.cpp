e88c1e13670cbe4ce5263980b168125954798cea
mkvarner <megan.varner63@gmail.com>
2014-06-24 19:35:40 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Hello_World_V1/main.cpp
@@ -0,0 +1,29 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner
+ * Created on June 24, 2014, 12:54 PM
+ * Purpose:First program example
+ */
+
+//System level libraries
+
+#include <iostream>
+using namespace std;
+
+//User defined libraries
+
+//Global constants i.e avogadro's number, conversions, speed of light, ect.
+
+//Function prototypes
+
+//Execution begins here
+int main(int argc, char** argv) {
+
+    //Output a string
+    
+    cout<<"Hello World"<<endl;
+    //Exit stage right
+    
+    return 0;
+}
+
