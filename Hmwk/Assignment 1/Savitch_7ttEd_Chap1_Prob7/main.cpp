5e7e149ae583570de733c0be5f4a355cd295f16c
mkvarner <megan.varner63@gmail.com>
2014-06-26 07:40:36 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Assignment 1/Savitch_7ttEd_Chap1_Prob7/main.cpp
@@ -0,0 +1,30 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner
+ * Created on June 25, 2014, 7:34 PM
+ * Purpose: Assignment 1
+ */
+
+#include <iostream>
+using namespace std;
+int main() 
+{
+    cout <<"*****************************************************\n";
+    cout <<"         C C C             S S S S          !!\n";
+    cout <<"       C      C          S        S         !!\n";
+    cout <<"      C                  S                  !!\n";
+    cout <<"     C                    S                 !!\n";
+    cout <<"     C                      S S S S         !!\n";
+    cout <<"     C                              S       !!\n";
+    cout <<"     C                                S     !!\n";
+    cout <<"      C       C            S         S        \n";
+    cout <<"        C C C                S S S S        OO\n";
+    cout <<"*****************************************************:\n";
+    cout <<"\n";
+    cout <<"\n";
+    cout <<"Computer Science is Cool Stuff!!!:\n";
+    
+    
+
+    return 0;
+}
