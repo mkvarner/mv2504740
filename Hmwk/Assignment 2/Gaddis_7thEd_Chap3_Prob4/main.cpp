e5e329b563e2b744acf9955563ff4262b77e0311
mkvarner <megan.varner63@gmail.com>
2014-06-29 15:38:05 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Assignment 2/Gaddis_7thEd_Chap3_Prob4/main.cpp
@@ -0,0 +1,44 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner
+ * Created on June 29, 2014, 3:00 PM
+ * Average rainfall for three months
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    char month_o, month_tw, month_th, sum, tot_avg, mo_rain_o, mo_rain_tw,
+            mo_rain_th;
+    
+    cout <<"Press enter after every entry.\n";
+    
+    cout <<"Enter name of first month:\n";
+    cin >> month_o;
+    cout <<"Enter the amount of rain that fell in inches:\n";
+    cin >> mo_rain_o;
+    
+    cout <<"Enter name of second month:\n";
+    cin >> month_tw;
+    cout <<"Enter the amount of rain that fell in inches:\n";
+    cin >> mo_rain_tw;
+    
+    cout <<"Enter name of third month:\n";
+    cin >> month_th;
+    cout <<"Enter amount of rain that fell in inches:\n";
+    cin >> mo_rain_th;
+    
+    //calculate average
+    sum = mo_rain_o + mo_rain_tw + mo_rain_th;
+    tot_avg = sum / 3;
+    
+    //End result
+    cout <<"The average rainfall for"<<month_o<<", "<<month_tw<<", and\n";
+    cout <<month_th<<" is "<<tot_avg<<" inches.\n";
+
+    return 0;
+}
+
