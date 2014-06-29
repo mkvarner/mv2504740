b7d33141152c537e65199efe436933726fb5ed57
mkvarner <megan.varner63@gmail.com>
2014-06-28 17:36:23 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Assignment 2/Gaddis_7thEd_Chap3_Prob2/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: cdarway
+ *
+ * Created on June 28, 2014, 5:13 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
