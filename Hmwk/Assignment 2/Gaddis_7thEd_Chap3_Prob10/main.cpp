a7c1aba682f0a8b74f0c5145f6831e147315fdc9
mkvarner <megan.varner63@gmail.com>
2014-06-30 20:06:14 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

--- Hmwk/Assignment 2/Gaddis_7thEd_Chap3_Prob10/main.cpp
+++ Hmwk/Assignment 2/Gaddis_7thEd_Chap3_Prob10/main.cpp
@@ -12,16 +12,19 @@ using namespace std;
 int main(int argc, char** argv) 
 {
     
-    double temp_C, temp_F, convert;
+    float temp_C, temp_F;
     cout << "Press enter after every number you type.\n";
     cout << "What is the temperature in Celsius?\n";
     cin >> temp_C;
     
-    convert = (9/5)*temp_C + 32;
+    temp_F = (9.0/5.0)* temp_C + 32.0;
     // gives miles per gallon
     
-    setprecsion(2);
     cout <<"\n";
+    
+    cout.setf(ios::fixed);
+    cout.precision(1);
+    
     cout <<"If it's " <<temp_C<<" degrees Celsius.\n";
     cout <<"Then, it's "<<temp_F<<" degrees Fahrenheit.\n";
             
