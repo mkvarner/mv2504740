5d9bd2a1252c775411a81232071e4bc3787b8aa0
mkvarner <megan.varner63@gmail.com>
2014-06-30 20:26:17 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

--- Hmwk/Assignment 2/Gaddis_7thEd_Chap3_Prob4/main.cpp
+++ Hmwk/Assignment 2/Gaddis_7thEd_Chap3_Prob4/main.cpp
@@ -11,38 +11,35 @@ using namespace std;
 
 int main(int argc, char** argv) 
 {
-    char month_o, month_tw, month_th, sum, tot_avg, mo_rain_o, mo_rain_tw,
-            mo_rain_th;
+    char month_o[10], month_tw[10], month_th[10];
+    float sum, tot_avg, mo_rain_o, mo_rain_tw, mo_rain_th;
     
     cout <<"Press enter after every entry.\n";
     
     cout <<"Enter name of first month:\n";
-    cin >> month_o;
-    cin.ignore();
+    cin.getline(month_o, 10);
     cout <<"Enter the amount of rain that fell in inches:\n";
     cin >> mo_rain_o;
-    cin.ignore();
     
     cout <<"Enter name of second month:\n";
     cin >> month_tw;
-    cin.ignore();
     cout <<"Enter the amount of rain that fell in inches:\n";
     cin >> mo_rain_tw;
-    cin.ignore();
     
     cout <<"Enter name of third month:\n";
     cin >> month_th;
-    cin.ignore();
     cout <<"Enter amount of rain that fell in inches:\n";
     cin >> mo_rain_th;
-    cin.ignore();
     
     //calculate average
     sum = mo_rain_o + mo_rain_tw + mo_rain_th;
     tot_avg = sum / 3;
     
     //End result
-    cout <<"The average rainfall for"<<month_o<<", "<<month_tw<<", and\n";
+    cout.setf(ios::fixed);
+    cout.precision(2);
+    cout <<"\n";
+    cout <<"The average rainfall for "<<month_o<<", "<<month_tw<<", and\n";
     cout <<month_th<<" is "<<tot_avg<<" inches.\n";
 
     return 0;
