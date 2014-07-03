c09eb174a481d57c465532b9e0da87bf06acc37b
mkvarner <megan.varner63@gmail.com>
2014-07-02 21:51:48 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Assignment 3/Gaddis_7thEd_Chap4_Prob1/main.cpp
@@ -0,0 +1,33 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner
+ * Created on July 2, 2014, 9:39 PM
+ * Which number is smaller and which is greater 
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    
+    float num1, num2, greater, lesser;
+    cout << "Press enter after every number you type.\n";
+    cout << "Enter first number:\n";
+    cin >> num1;
+    
+    cout << "Enter second number:\n";
+    cin >> num2;
+    
+    
+    greater = num1 >num2 ? num1 : num2;
+    lesser = num1 <num2 ? num1 : num2;
+    
+    cout <<greater<<" is greater than "<<lesser<<endl;
+            
+            
+
+    return 0;
+}
