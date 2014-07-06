fd20641b6ed2ee3177bf6df626cddfff453f295c
mkvarner <megan.varner63@gmail.com>
2014-07-06 14:57:57 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

--- Hmwk/Assignment 3/Savitch_7thEd_Chap3_Prob2/main.cpp
+++ Hmwk/Assignment 3/Savitch_7thEd_Chap3_Prob2/main.cpp
@@ -1,19 +1,72 @@
 /* 
  * File:   main.cpp
- * Author: cdarway
- *
- * Created on July 6, 2014, 12:53 PM
+ * Author: Megan Varner
+ * Created on July 2, 2014, 9:39 PM
+ * Switch number to roman numeral
  */
 
-#include <cstdlib>
+#include <iostream>
 
 using namespace std;
+int main(int argc, char** argv)
+{
+ int numberIn;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+cout << "Press enter after every entry.\n";
+cout << "Enter number 1-10 and Roman numeral conversion will appear:\n";
+cin >> numberIn;
 
-    return 0;
+// Display roman numeral
+switch(numberIn)
+{
+
+case '1': 
+    cout << "I\n";
+break;
+
+
+case '2':
+    cout << "II\n";
+break;
+
+
+case '3':
+    cout << "III\n";
+break;
+
+
+case '4': 
+    cout << "IV\n";
+break;
+
+
+case '5':
+    cout << "V\n";
+break;
+
+
+case '6': 
+    cout << "VI\n";
+break;
+
+
+case '7':
+    cout << "VII\n";
+break;
+
+
+case '8':
+    cout << "VIII\n";
+break;
+
+
+case '9':
+    cout << "IX\n";
+break;
+
+default:
+    cout << "That is an invalid choice.\n";
+
 }
-
+return 0;
+}
