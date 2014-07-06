ba052d71881bb527e7b678209b21d9b68bf90802
mkvarner <megan.varner63@gmail.com>
2014-07-06 15:35:35 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Assignment 3/Gaddis_7thEd_Chap4_Prob7/main.cpp
@@ -0,0 +1,46 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner
+ * Created on July 2, 2014, 9:39 PM
+ * Seconds, minutes, hours conversion
+ */
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, char** argv) 
+{
+    
+    float num1, minute, hour, day;
+    cout << "Press enter after every number you type.\n";
+    cout << "Enter number of seconds:\n";
+    cin >> num1;
+    
+    minute = num1/60;
+    hour = num1/3600;
+    day = num1/86400;
+    
+    cout<<"\n";
+    if (num1 < 60)
+        cout <<num1<<" seconds.";
+    
+    else if (num1 <3600&& num1 >= 60)
+        cout <<minute<<" minutes.";
+    
+    else if (num1 < 86400&& num1 >= 3600)
+        cout <<hour<<" hours.";
+        
+    else if (num1 >= 86400)
+        cout <<day<<" days.";
+             
+    else
+        cout <<"Invalid entry";
+       
+    cout <<"\n";
+    
+    
+    
+    return 0;
+}
