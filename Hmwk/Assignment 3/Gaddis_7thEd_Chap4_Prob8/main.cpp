0e26f9ca7da8080d8d6a5bade256c9747b2ac5b8
mkvarner <megan.varner63@gmail.com>
2014-07-06 16:00:51 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Assignment 3/Gaddis_7thEd_Chap4_Prob8/main.cpp
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner
+ * Created on June 25, 2014, 7:34 PM
+ * Enter amount of quarters,dimes, and 
+ * input money amounts output less, equal, or more than a dollar
+ */
+
+#include <iostream>
+#include<iomanip>
+
+using namespace std;
+int main(int argc, char** argv) 
+{
+    float quart, dim, nickel, p, tot_quart, pt, tot_dim, tot_nickel, tot_amount;
+    cout << "Press return after entering a number.\n";
+    cout << "Enter the number of quarters:\n";
+    cin >> quart;
+    cout << "Enter the number of dimes:\n" ;
+    cin >> dim;
+    cout << "Enter the number of nickels:\n";
+    cin >> nickel;
+    cout << "Enter the number of pennies:\n";
+    cin >> p;
+    
+    
+    tot_quart = quart * 0.25;
+    tot_dim = dim * 0.10;
+    tot_nickel = nickel * 0.05;
+    pt = p * 0.01;
+    tot_amount = tot_quart + tot_dim + tot_nickel + pt;
+  
+    
+    if (tot_amount < 1)
+        cout <<"Less than one dollar.";
+    
+    else if (tot_amount == 1)
+        cout <<"Congratulations you WIN!";
+    
+    else if (tot_amount > 1)
+        cout<<"More than one dollar.";
+    
+    else
+        cout <<"Invalid entry";
+    
+      cout<<"\n";
+            
+
+    return 0;
+}
