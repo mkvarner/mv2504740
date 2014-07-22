c0b4cc3036579a99232d37601464f43fec8e6be5
mkvarner <megan.varner63@gmail.com>
2014-07-22 16:19:26 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

--- Hmwk/Assignment 5/number1/main.cpp
+++ Hmwk/Assignment 5/number1/main.cpp
@@ -1,19 +1,49 @@
 /* 
  * File:   main.cpp
- * Author: cdarway
- *
- * Created on July 22, 2014, 3:38 PM
+ * Author: Megan Varner
+ * Created on July 21, 2014, 12:42 PM
  */
 
+//System Libraries
 #include <cstdlib>
-
+#include <ctime>
+#include <iostream>
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
+//Global constants
 
+//Function Prototypes
+
+float calculateRetail(const int [4],int);
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    
+    //Declare the array and its maximum size
+    const int MAXSZ=4;
+    float cost, mark, total;
+    
+    //Set the random number seed
+    srand(static_cast<unsigned int>(time(0)));
+    
+    //Prompt for inputs
+    cout<<"Item's wholesale cost: "<<endl;
+    cin>>cost;
+    cout<<"markup percent: "<<endl;
+    cin>>mark;
+    
+    //Calculate the number of columns
+   
+mark = mark * .01;
+
+//This is the function call
+total = calculateRetail(cost, mark);
+    
+    
+    //Output some interesting information
+    cout<<"Retail value is & "<<calculateRetail(cost,mark)<<endl;
+   
+    
+    //Exit stage right!
     return 0;
 }
-
