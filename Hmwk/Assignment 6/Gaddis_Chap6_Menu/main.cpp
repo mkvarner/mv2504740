0c0e33928b80edb2bc41690c1419db30f89e18e2
mkvarner <megan.varner63@gmail.com>
2014-07-24 19:36:37 -0700

    Signed-off-by: mkvarner <megan.varner63@gmail.com>

---
+++ Hmwk/Assignment 6/Gaddis_Chap6_Menu/main.cpp
@@ -0,0 +1,61 @@
+/* 
+ * File:   main.cpp
+ * Author: Megan Varner 
+ * Created on July 24, 2014, 7:32 PM
+ */
+
+//System Libraries
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Function Prototypes
+
+//Execution Starts Here!
+int main(int argc, char** argv) {
+    
+    //Declare Variables
+    char choice;
+    
+    //Loop until choice is not in the menu selection
+    do{
+        //Output the menu and input the choice
+        cout<<"Type 1 for Problem X from Gaddis"<<endl;
+        cout<<"Type 2 for Problem X from Gaddis"<<endl;
+        cout<<"Type 3 for Problem X from Gaddis"<<endl;
+        cout<<"Type 4 for Problem B from Gaddis"<<endl;
+        cout<<"Type 5 for Problem B from Gaddis"<<endl;
+        cout<<"Type 6 for Problem B from Gaddis"<<endl;
+        cout<<"Type 7 for Problem B from Gaddis"<<endl;
+        cout<<"Type 8 for Problem B from Gaddis"<<endl;
+        cout<<"Type 9 for Problem B from Gaddis"<<endl;
+        cout<<"Type 10 for Problem B from Gaddis"<<endl;
+        cin>>choice;
+        
+        //Place problem solutions in the switch statement
+        cout<<endl;
+        switch(choice){
+            case  '1':{
+                cout<<"Problem X here!"<<endl<<endl;
+                break;
+            }case '2':{
+                cout<<"Problem Y here!"<<endl<<endl;
+                break;
+            }case '3':{
+              
+                break;         
+            }case '4':{
+                cout<<"Problem B here!"<<endl<<endl;
+                break;
+            }default:{
+                cout<<"Exit Menu"<<endl<<endl;
+            }
+        };
+    }while(choice>=1&&choice<=10);
+
+    //Exit stage right!
+    return 0;
+}
